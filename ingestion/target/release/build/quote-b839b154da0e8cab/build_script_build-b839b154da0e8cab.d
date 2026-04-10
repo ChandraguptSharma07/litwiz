62125b410cc58bce7493e9171c75243a9b1dd4ee
hy69x <himanshu809809@gmail.com>
2026-04-10 15:34:53 +0530

    Ingesion v2

--- ingestion/target/release/build/quote-b839b154da0e8cab/build_script_build-b839b154da0e8cab.d
+++
@@ -1,5 +0,0 @@
-/home/hy/code/projects/litwiz/ingestion/target/release/build/quote-b839b154da0e8cab/build_script_build-b839b154da0e8cab.d: /home/hy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/home/hy/code/projects/litwiz/ingestion/target/release/build/quote-b839b154da0e8cab/build_script_build-b839b154da0e8cab: /home/hy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs
-
-/home/hy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/quote-1.0.45/build.rs:
--- ingestion/target/release/build/zmij-ff24151a455a94b2/build_script_build-ff24151a455a94b2.d
+++
@@ -1,5 +0,0 @@
-/home/hy/code/projects/litwiz/ingestion/target/release/build/zmij-ff24151a455a94b2/build_script_build-ff24151a455a94b2.d: /home/hy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.21/build.rs
-
-/home/hy/code/projects/litwiz/ingestion/target/release/build/zmij-ff24151a455a94b2/build_script_build-ff24151a455a94b2: /home/hy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.21/build.rs
-
-/home/hy/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/zmij-1.0.21/build.rs:
